254073330a89cb5a495081b86af9d5b8d888b152
Jon West <jon.c.west@outlook.com>
2023-09-06 17:03:45 -0500

    init

---
+++ main.tf
@@ -0,0 +1,58 @@
+locals {
+  sns_topic_name              = var.topic_name || "sns-topic"
+  enable_delivery_status_logs = var.enable_delivery_status_logs
+  create_sns_delivery_role    = var.enable_delivery_status_logs && var.delivery_status_lambda_role_arn == ""
+  sns_feedback_role           = local.create_sns_delivery_role ? aws_iam_role.sns_delivery_role[0].arn : var.delivery_status_lambda_role_arn
+
+  lambda_failure_feedback_role_arn    = local.enable_delivery_status_logs ? local.sns_feedback_role : null
+  lambda_success_feedback_role_arn    = local.enable_delivery_status_logs ? local.sns_feedback_role : null
+  lambda_success_feedback_sample_rate = local.enable_delivery_status_logs ? var.delivery_status_lambda_sample_rate : null
+}
+
+## SNS Topic
+resource "aws_sns_topic" "this" {
+  name = local.sns_topic_name
+
+  kms_master_key_id = var.sns_topic_kms_key_id
+
+  lambda_failure_feedback_role_arn    = local.lambda_failure_feedback_role_arn
+  lambda_success_feedback_role_arn    = local.lambda_success_feedback_role_arn
+  lambda_success_feedback_sample_rate = local.lambda_success_feedback_sample_rate
+
+  tags = var.tags
+}
+
+## IAM
+data "aws_iam_policy_document" "sns_delivery" {
+  count = local.create_sns_delivery_role ? 1 : 0
+
+  statement {
+    sid    = "PermitDeliveryStatusMessagesToCloudWatchLogs"
+    effect = "Allow"
+
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents",
+      "logs:PutMetricFilter",
+      "logs:PutRetentionPolicy"
+    ]
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "sns_delivery_role" {
+  count = local.create_sns_delivery_role ? 1 : 0
+
+  name                  = var.delivery_status_role_name
+  description           = var.delivery_status_role_description
+  path                  = var.delivery_status_role_path
+  force_detach_policies = var.delivery_status_role_force_detach_policies
+  permissions_boundary  = var.delivery_status_role_permissions_boundary
+  assume_role_policy    = data.aws_iam_policy_document.sns_delivery[0].json
+
+  tags = merge(var.tags, var.delivery_status_role_tags)
+}
---
+++ variables.tf
@@ -0,0 +1,71 @@
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "topic_name" {
+  description = "The name of the SNS topic to create"
+  type        = string
+}
+
+variable "sns_topic_kms_key_id" {
+  description = "ARN of the KMS key used for enabling SSE on the topic"
+  type        = string
+  default     = ""
+}
+
+variable "enable_delivery_status_logs" {
+  description = "Whether to enable SNS delivery status logs"
+  type        = bool
+  default     = false
+}
+
+variable "delivery_status_lambda_role_arn" {
+  description = "IAM role for SNS delivery status logs.  If this is set then a role will not be created for you."
+  type        = string
+  default     = ""
+}
+
+
+variable "delivery_status_lambda_sample_rate" {
+  description = "The percentage of successful deliveries to log"
+  type        = number
+  default     = 100
+}
+
+variable "delivery_status_role_name" {
+  description = "Name of the IAM role to use for SNS delivery status logging"
+  type        = string
+  default     = null
+}
+
+variable "delivery_status_role_description" {
+  description = "Description of IAM role to use for SNS delivery status logging"
+  type        = string
+  default     = null
+}
+
+variable "delivery_status_role_path" {
+  description = "Path of IAM role to use for SNS delivery status logging"
+  type        = string
+  default     = null
+}
+
+variable "delivery_status_role_force_detach_policies" {
+  description = "Specifies to force detaching any policies the IAM role has before destroying it."
+  type        = bool
+  default     = true
+}
+
+variable "delivery_status_role_permissions_boundary" {
+  description = "The ARN of the policy that is used to set the permissions boundary for the IAM role used by SNS delivery status logging"
+  type        = string
+  default     = null
+}
+
+variable "delivery_status_role_tags" {
+  description = "A map of tags to assign to IAM the SNS delivery status role"
+  type        = map(string)
+  default     = {}
+}
